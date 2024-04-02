ac063bf01b90f5c87d9f13fa9b20067227268bd8
ckouder <bingjiguo@icloud.com>
2024-04-01 21:38:30 -0500

    add color indicator

--- src/app/components/somn/somn/somn.component.ts
+++ src/app/components/somn/somn/somn.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from "@angular/core";
+import * as d3 from "d3";
 import { FilterService, MenuItem } from "primeng/api";
 import { Table } from "primeng/table";
 import { BehaviorSubject, combineLatest, map, of } from "rxjs";
@@ -34,6 +35,29 @@ export class SomnComponent {
   selectedBases$ = new BehaviorSubject<any[]>([]);
   selectedSolvents$ = new BehaviorSubject<any[]>([]);
   selectedYield$ = new BehaviorSubject([0, 100]);
+
+  dataWithColor$ = this.response$.pipe(
+    map((response) => 
+      response.data.map((d) => ({ 
+        ...d, 
+        color: this.getColorAtPercentage(
+          d["yield"], 
+          d3.min(response.data, d => d["yield"])!, 
+          d3.max(response.data, d => d["yield"])!
+        )
+      }))
+    ),
+  );
+
+  topYieldConditions$ = this.dataWithColor$.pipe(
+    map((data) => {
+      const yields = Math.max(...data.map((d) => Math.floor(d["yield"] * 100)));
+      return {
+        topYield: yields,
+        conditions: data.filter((d) => Math.floor(d["yield"] * 100) >= yields),
+      }
+    }),
+  );
 
   filteredDataWithoutYieldRange$ = combineLatest([
     this.response$,
@@ -110,6 +134,21 @@ export class SomnComponent {
     this.selectedCatalysts$.next([]);
     this.selectedSolvents$.next([]);
     this.resultsTable.reset();
+  }
+
+  getColorAtPercentage(percentage: number, min: number, max: number) {
+    percentage = percentage > 1 ? percentage / 100 : percentage;
+    const colorStops = [
+      { offset: "0%", color: "#470459" },
+      { offset: "50%", color: "#2E8C89" },
+      { offset: "100%", color: "#F5E61D" }
+    ];
+
+    // Create a scale to map the percentage to the corresponding color stop
+    const scale = d3.scaleLinear(colorStops.map(stop => stop.color))
+      .domain(colorStops.map(stop => parseFloat(stop.offset)));
+
+    return scale((percentage - min) / (max - min) * 100);
   }
 
   onYieldRangeChange(value: [number, number]) {
